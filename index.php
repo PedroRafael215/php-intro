e12a1b8f9ccd0006cde860fafde908e1a4a93125
PedroRafael215 <pedro.rafael215@gmail.com>
2022-02-01 08:42:14 -0300

    Adicionando comentários ao código

--- index.php
+++ index.php
@@ -14,25 +14,30 @@ session_start();
 </head>
 
 <body>
-<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
-<form action="script.php" method="post">
-	<?php
-		$mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
-		if(!empty($mensagemDeErro)){
-			echo $mensagemDeErro;
-		}
+	<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
-		$mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : '';
-		if(!empty($mensagemDeSucesso)){
-			echo $mensagemDeSucesso;
-		}
+		<form action="script.php" method="post">
+			<?php
+				$mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';   //Se um valor no array de SESSION de nome 'mensagem-de-erro' está ativo, atribui esse valor à variavel, se não, atribui ''(espaço vazio)  .
+				
+				if(!empty($mensagemDeErro)){// se a variavel $mensagemDeErro não estiver 'vazia', então executa o comando (contendo a string definida em script.php)
+					echo $mensagemDeErro;
+				}
 
-	?>
+				$mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : '';//Se um valor no array de SESSION de nome 'mensagem-de-sucesso' está ativo, atribui esse valor à variavel, se não, atribui ''(espaço vazio)  .
+				
+				if(!empty($mensagemDeSucesso)){ //se a variavel$mensagemDeSucesso não estiver 'vazia', então executa o comando (contendo a string definida em script.php)
+					echo $mensagemDeSucesso;
+				}
 
-	<p>Seu nome: <input type="text" name="nome"> </p>
-	<p>Sua idade: <input type="text" name="idade"></p>
-	<p><input type="submit" value="Enviar dados do competidor"></p>
-</form>
+			?>
+
+			<p>Seu nome: <input type="text" name="nome"> </p>
+			<p>Sua idade: <input type="text" name="idade"></p>
+			<p><input type="submit" value="Enviar dados do competidor"></p>
+
+		</form>
+
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,44 +1,54 @@
 <?php
 
-session_start();
+session_start(); //starta a sessão
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
+$categorias = [];                     	//Cria array que será preenchido com o nome das modalidades
 
-
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
+$categorias[] = 'infantil';				//Atribuindo modalidade ao array
+$categorias[] = 'adolescente';			//*
+$categorias[] = 'adulto';				//*
 
 
+$nome = $_POST['nome'];					//Recupera dados do formulário HTML de name='nome'
+$idade = $_POST['idade'];				//Recupera dados do formulário HTML de name='idade'
+
+
+
+//Tratamento de condições formulário
+
+
+// Se o campo nome estiver vazio:
 if(empty($nome)){
 	$_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio. por favor, preencha-o novamente';
 	header('location: index.php');
 	return;
 }
 
+//Se a quantidade de caracteres no campo nome for menor que 3:
 else if(strlen($nome) < 3){
 	$_SESSION['mensagem-de-erro'] = 'O nome deve conter mais de 3 caracteres';
 	header('location: index.php');	
 	return;	
 }
 
+//Se a quantidade de caracteres no campo nome for maior que 40:
 else if(strlen($nome) > 40){
 	$_SESSION['mensagem-de-erro'] = 'O nome é muito extenso';
 	header('location: index.php');	 
 	return;
 }
 
+// Se o valor digitado no campo idade for diferente de numérico:
 else if(!is_numeric($idade)){
 	$_SESSION['mensagem-de-erro'] = 'Informe um número para a idade';
 	header('location: index.php');	
 	return;
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//Resolução do problema (Se idade X, modalidade de natação é Y):
 
-
-
+//1
 else if($idade >= 6 && $idade <= 12){
 	
 	for($i=0; $i <= count($categorias); $i++){
@@ -51,7 +61,7 @@ else if($idade >= 6 && $idade <= 12){
 			
 	
 
-
+//2
 }else if($idade >= 13 && $idade <= 18){
 	for($i=0; $i <= count($categorias); $i++){
 		if($categorias[$i] == 'adolescente'){
@@ -61,6 +71,7 @@ else if($idade >= 6 && $idade <= 12){
 		}
 	}
 }else{
+//3	
 	for($i=0; $i <= count($categorias); $i++){
 		if($categorias[$i] == 'adulto'){
 			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
